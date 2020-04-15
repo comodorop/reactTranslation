4d1456c683cc27b64e53016a6a8b7646cd36eb5a
Torres <comodoro49@gmail.com>
2020-04-15 15:39:13 -0500

    Add translations

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import i18n from './config/i18n';
+import { I18nextProvider } from 'react-i18next';
 
 ReactDOM.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+
+  <I18nextProvider i18n={i18n}>
     <App />
-  </React.StrictMode>,
+  </I18nextProvider>,
+  // </React.StrictMode>,
   document.getElementById('root')
 );
 
